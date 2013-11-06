9ca997d41eb4fba602be0c7d6f2dc37274659b5d
Joao Nuno Silva <joao.silva@feedzai.com>
2013-11-06 17:12:24 +0000

    Add tests to cover all function with and without context.

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
+'use strict';
+
 var _ = require('underscore');
 
-exports.dot = function dot(name) {
-	return function(o) {
-		return o[name];
-	}
+function allowContext(fn) {
+  fn.on = function (ctx) {
+    return _.bind(fn, ctx);
+  };
+  return fn;
 }
 
-exports.callWith = function callWith() {
-	var args = _.toArray(arguments);
-	return function (fn) {
-		fn.apply(this, args);
-	};
-}
+exports.dot = function (name) {
+  return function (o) {
+    return o[name];
+  };
+};
 
-exports.applyWith = function applyWith() {
-	var args = _.toArray(arguments);
-	return function (fn) {
-		fn.apply(this, _.first(args));
-	};
-}
+exports.callWith = function () {
+  var args = _.toArray(arguments);
+  return allowContext(function (fn) {
+    return fn.apply(this, args);
+  });
+};
 
-exports.apply = function apply(fn) {
-	return function () {
-		fn.apply(this, _.first(_.toArray(arguments)));
-	};
-}
+exports.applyWith = function () {
+  var args = _.toArray(arguments);
+  return allowContext(function (fn) {
+    return fn.apply(this, _.first(args));
+  });
+};
+
+exports.call = function (fn) {
+  return allowContext(function () {
+    return fn.apply(this, _.toArray(arguments));
+  });
+};
+
+exports.apply = function (fn) {
+  return allowContext(function () {
+    return fn.apply(this, _.first(_.toArray(arguments)));
+  });
+};
--- test/test.js
+++ test/test.js
@@ -1,17 +1,76 @@
-/* global describe, it, beforeEach */
+/* global describe, it */
+'use strict';
 
-var chai, assert, sinon, spy, futil;
+var chai, assert, futil, _;
 chai = require('chai');
 futil = require('..');
+_ = require('underscore');
 assert = chai.assert;
 chai.Assertion.includeStack = true;
 
 describe('futil', function () {
-  'use strict';
+  _.mixin(futil);
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  function sumUsingContext(a) {
+    /* jshint validthis: true */
+    return a + this.b;
+  }
 
   describe('dot', function () {
     it('shoud get the property', function () {
-      assert.equal('value', futil.dot('name')({name: 'value'}));
+      assert.equal('value', _.dot('name')({name: 'value'}));
+    });
+  });
+
+  describe('callWith', function () {
+    it('should call the function', function () {
+      assert.equal(5, _.callWith(2, 3)(sum));
+    });
+  });
+
+  describe('callWith on', function () {
+    it('should call the function on the given context', function () {
+      assert.equal(5, _.callWith(2).on({b: 3})(sumUsingContext));
+    });
+  });
+
+  describe('applyWith', function () {
+    it('should apply the function', function () {
+      assert.equal(5, _.applyWith([2, 3])(sum));
+    });
+  });
+
+  describe('applyWith on', function () {
+    it('should apply the function on the given context', function () {
+      assert.equal(5, _.applyWith([2]).on({b: 3})(sumUsingContext));
+    });
+  });
+
+  describe('call', function () {
+    it('should call the function with the given args', function () {
+      assert.equal(5, _.call(sum)(2, 3));
+    });
+  });
+
+  describe('call on', function () {
+    it('should call the function on the given context with the given args', function () {
+      assert.equal(5, _.call(sumUsingContext).on({b: 3})(2));
+    });
+  });
+
+  describe('apply', function () {
+    it('should apply the function with the given args', function () {
+      assert.equal(5, _.apply(sum)([2, 3]));
+    });
+  });
+
+  describe('apply on', function () {
+    it('should apply the function on the given context with the given args', function () {
+      assert.equal(5, _.apply(sumUsingContext).on({b: 3})([2]));
     });
   });
 });
